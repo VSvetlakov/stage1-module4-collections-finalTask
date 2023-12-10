70b5ea7103a9cc1b2a917c50de6563d1b36abbe5
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-12-10 21:50:03 +0400

    v.1

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,37 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+        List<String> strings = new ArrayList<>();
+
+        for (Map.Entry<String,Set<String>> entry: projects.entrySet()) {
+
+            String project = entry.getKey();
+
+            Set<String> developers = entry.getValue();
+
+            if (developers.contains(developer)){
+                strings.add(project);
+            }
+        }
+
+        Collections.sort(strings, new ProjectComparator());
+
+        return strings;
+    }
+
+    class ProjectComparator implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+
+            if (o2.length() == o1.length())
+                return o2.compareTo(o1);
+
+            return o2.length() - o1.length();
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+
+        Map<Integer, Set<String>> setMap = new HashMap<>();
+
+        for (Map.Entry<String,Integer> entry: sourceMap.entrySet()) {
+
+            String key = entry.getKey();
+            Integer lengthKey = key.length();
+
+            if (setMap.containsKey(lengthKey)){
+                setMap.get(lengthKey).add(key);
+            }else {
+                Set<String> strings = new HashSet<String>();
+                strings.add(key);
+                setMap.put(lengthKey,strings);
+            }
+        }
+
+        return setMap;
     }
 }
